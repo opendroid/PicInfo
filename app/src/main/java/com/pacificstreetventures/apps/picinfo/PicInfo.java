e70701064d95bab948566e49b70fb33f56951ae5
opendriod <ajaythakur@hotmail.com>
2015-04-27 22:37:44 -0700

    Ongoing evolution. Added headless ReatinedFragement that uploads data from image databases (INTERBAL and EXTERNAL) using Loader<cursors>

--- app/src/main/java/com/pacificstreetventures/apps/picinfo/PicInfo.java
+++ app/src/main/java/com/pacificstreetventures/apps/picinfo/PicInfo.java
@@ -8,20 +8,18 @@
  */
 
 package com.pacificstreetventures.apps.picinfo;
-
-import android.graphics.Bitmap;
-import android.graphics.ImageFormat;
-import android.media.ExifInterface;
 import android.net.Uri;
-import android.provider.MediaStore;
 import android.util.Log;
-import android.util.Rational;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Locale;
 import java.util.UUID;
 
 /**
@@ -30,267 +28,169 @@ import java.util.UUID;
 public class PicInfo {
     private final String TAG = getClass().getSimpleName();
 
-    private UUID  mImageId; // Unqiue image ID traversing the list
+    private UUID mImageId; // Unique image ID traversing the list
     private Uri mImageFileName; // As provided by Content Provider
-    private byte[] mImageThumbNail160x160;
-    private ImageFormat mImageFormat;
-    private ExifInterface mExifInterfaceData; // Exit Iterface data
-    private ImageColumnsInDb mColumnsInDb;
+    private JpegExifData mJpegExifData;
+    private Metadata mImageMetaData; // Exit Interface data
+    private ImageDbColumns mColumnsInDb;
+    private String mError;
 
     public PicInfo() {
         mImageId = UUID.randomUUID();
         mImageFileName = null;
-        mExifInterfaceData = null;
-        mImageThumbNail160x160 = null;
-
-        mColumnsInDb = new ImageColumnsInDb();
-        return;
+        mImageMetaData = null;
+        mJpegExifData = null;
+        mColumnsInDb = new ImageDbColumns();
+        mError = null;
     }
 
     // Store pic info
-    public void ProcessExifPicInfo(Uri filename) {
+    public void processImageMetaData(final String filename) {
         // Image UUID
         // mImageId = UUID.randomUUID(); // Generate a random UUID
-        mImageFileName = Uri.parse(filename.toString());
-        mExifInterfaceData = null;
-        mImageThumbNail160x160 = null;
-        //
+
+        mImageFileName = Uri.parse(filename);
+        mImageMetaData = null;
+
         // Get EXIF data ImageFormat. ImageFormat.JPEG
         // Get mimetype 'mimeType.equalsIgnoreCase("image/jpeg"'
         // MediaStore.Images.Media.CONTENT_TYPE has value 'image/jpeg'
-        //
-        //
         if (filename != null) {
-            File file = new File(filename.toString());
-            if (file.exists()) {
-                try {
-                    // Get Image Format
-                    // mImageFormat = new ImageFormat(filename);
-                    //
-                    // Save EXIF data
-                    mExifInterfaceData = new ExifInterface(filename.toString());
-                    mImageThumbNail160x160 = mExifInterfaceData.getThumbnail(); // blocking call
-                } catch (IOException ex) {
-                    Log.e(TAG, "Cant read EXIF for " + filename);
+            File imageFile = new File(filename);
+            if (imageFile.exists()) {
+                // Use Android Library to get EXIF info.
+                // Use mime type from database not image itself ('easy')
+                if ((mColumnsInDb != null) &&
+                        (mColumnsInDb.getMimeType() != null) &&
+                        mColumnsInDb.getMimeType().toLowerCase(Locale.US).contains("jpeg")) {
+                    fetchExifDataForJpeg(filename);
                 }
-            }
-        } // end if
+
+                // Use library metadata-extractor-2.8.1.jar and 'xmpcore-5.1.2.jar'
+                try {
+                    mImageMetaData = ImageMetadataReader.readMetadata(imageFile);
+                    mError = null;
+                } catch (ImageProcessingException ex) {
+                    mImageMetaData = null;
+                    mError = "\n[IMAGEDB] Error Image processing exception on: " + filename;
+                    Log.e(TAG, "ProcessingException: filename:" + filename + "\n", ex);
+                    Log.d(TAG, "MetaData:" + this.toString());
+                } catch (IOException ex) {
+                    Log.e(TAG, "IOException: filename:" + filename + "\n", ex);
+                    mImageMetaData = null;
+                    mError = "\n[IMAGEDB] Error Can not read image: " + filename;
+                    // Log.d(TAG, "MetaData:" + this.toString());
+                }
+
+            } // end if 'imageFile.exists()'
+        } // end if 'filename != null'
     }
 
-    // EXIF info
-    private class ExifJpegData {
-        // Picture -- taken data.
-        private String      mAperture;
-        private String      mDatetime;
-        private String      mExposureTime;
-        private Integer     mFlash;
-        private Rational    mFocus;
-        private Integer     mCameraOrientation;
-        private Integer     mWhiteBalance;
-
-        // GOS Data -- times.
-        private Integer     mGpsAltitude;
-        private String      mGpsDateStamp;
-        private String      mGpsTimeStamp;
-        private String      mGpsLongitude;
-        private String      mGpsLatitude;
-        private String      mGpsLatitudeRef;
-        private String      mGpsLongitudeRef;
-        private Integer     mGpsAltitudeRef;
-        private String      mGpsProcessingMethod;
-
-        // Camera data -- times.
-        private String      mCameraIso;
-        private String      mCameraMake;
-        private String      mCameraModel;
-
-        public ExifJpegData() {
-            // CLean object data state
-            mAperture = mDatetime = mExposureTime = null;
-            mGpsDateStamp = mGpsTimeStamp = mGpsLongitude = mGpsLatitude = null;
-            mGpsLatitudeRef = mGpsLongitudeRef = null;
-            mCameraIso = mCameraMake = mCameraModel = null;
-            mFlash = mCameraOrientation = mWhiteBalance = -1;
-            mGpsAltitude = mGpsAltitudeRef = 0;
-            return;
-        } // Constructor
+    // Helper function.
+    private void fetchExifDataForJpeg(final String filename) {
+        try {
+            mJpegExifData = new JpegExifData(filename);
+            // Log.d(TAG,"fetchExifDataForJpeg, TAG infor for file: " + filename);
+            // Log.e(TAG,"Data:" + mJpegExifData);
+        } catch (IOException | OutOfMemoryError ex) {
+            mJpegExifData = null;
+        }
     }
 
-    // Columns read in file. -- same name as in database.
-    // @link https://android.googlesource.com/platform/packages/apps/Gallery2/+/jb-dev/src/com/android/gallery3d/data/LocalAlbumSet.java
-    // for explanation
-    class ImageColumnsInDb {
-        private long mColumns; // How many columns data is available
-        private long _id;
-        // private byte[]  _data;
-        private long _size;
-        private String _display_name;
-        private String mime_type;
-        private String title;
-        private Date date_added;
-        private Date date_modified;
-        private String description;
-        private String picasa_id;
-        private int isprivate;
-        private double latitude;
-        private double longitude;
-        private Date datetaken;
-        private int orientation;
-        private int mini_thumb_magic;
-        private String bucket_id;
-        private String bucket_display_name;
-        private int width;
-        private int height;
-
-        /**
-         * Note found these extra columns on Samsung note 3
-         *
-         * group_id, spherical_mosaic, addr, langagecode, is_secretbox,
-         * weather_ID, sef_file_type, reusable]
-         *
-         */
-
-        // Setters.
-        public void setID(long id) {
-            _id = id;
-        }
-        public void setSize(long sz) {
-            _size = sz;
-        }
-        public void setDisplayName (String displayName) {
-            _display_name = displayName;
-        }
-        public void setMimeType (String mimeType) {
-            mime_type  = mimeType;
-        }
-        public void setTitle (String title) {
-            this.title = title;
-        }
-        public void setDateAdded (long dateAddedMilliSec) {
-            date_added = new Date(dateAddedMilliSec);
-        }
-        public void setDateModified (long dateModifiedMilliSec) {
-            date_modified = new Date(dateModifiedMilliSec);
-        }
-        public void setDateTake (long dateTakenMilliSec) {
-            datetaken = new Date(dateTakenMilliSec);
-        }
-        public void setDescription (String description) {
-            this.description = description;
-        }
-        public void setPicasaId (String id) {
-            this.picasa_id = id;
-        }
-        public void setPrivate (int isprivate) {
-            this.isprivate = isprivate;
-        }
-        public void setLocation(double _latitide, double _longitude) {
-            latitude = _latitide;
-            longitude = _longitude;
-        }
-        public void setWidthHeight(int _width, int _height) {
-            width = _width;
-            height = _height;
-        }
-        public void setOrientation (int orientation) {
-            this.orientation = orientation;
-        }
-        public void setBucket (String bucketId, String bucketDisplayName) {
-            bucket_id = bucketId;
-            bucket_display_name = bucketDisplayName;
-        }
-        public void setThumbnail(int thumbnail) {
-            mini_thumb_magic = thumbnail;
-        }
-    } // End inner class
 
     // Accessors to set Inner class:
-    public void setColumns (long nColumns) {
-        mColumnsInDb.mColumns = nColumns;
+    public void setColumns(int nColumns) {
+        mColumnsInDb.setColumns(nColumns);
     }
     public void setID(long id) {
-        mColumnsInDb._id = id;
+        mColumnsInDb.setID(id);
     }
     public void setSize(long sz) {
-        mColumnsInDb._size = sz;
+        mColumnsInDb.setSize(sz);
     }
     public void setDisplayName (String displayName) {
-        mColumnsInDb._display_name = displayName;
+        mColumnsInDb.setDisplayName(displayName);
     }
     public void setMimeType (String mimeType) {
-        mColumnsInDb.mime_type  = mimeType;
+        mColumnsInDb.setMimeType(mimeType);
     }
     public void setTitle (String title) {
-        mColumnsInDb.title = title;
+        mColumnsInDb.setTitle(title);
     }
     public void setDateAdded (long dateAddedMilliSec) {
-        mColumnsInDb.date_added = new Date(dateAddedMilliSec);
+        mColumnsInDb.setDateAdded(dateAddedMilliSec);
     }
     public void setDateModified (long dateModifiedMilliSec) {
-        mColumnsInDb.date_modified = new Date(dateModifiedMilliSec);
+        mColumnsInDb.setDateModified(dateModifiedMilliSec);
     }
     public void setDateTaken (long dateTakenMilliSec) {
-        mColumnsInDb.datetaken = new Date(dateTakenMilliSec);
+        mColumnsInDb.setDateTakenMilliSec(dateTakenMilliSec);
     }
     public void setDescription (String description) {
-        mColumnsInDb.description = description;
+        mColumnsInDb.setDescription(description);
     }
     public void setPicasaId (String id) {
-        mColumnsInDb.picasa_id = id;
+        mColumnsInDb.setPicasaId(id);
     }
     public void setPrivate (int isprivate) {
-        mColumnsInDb.isprivate = isprivate;
+        mColumnsInDb.setPrivate(isprivate);
     }
     public void setLocation(double _latitide, double _longitude) {
-        mColumnsInDb.latitude = _latitide;
-        mColumnsInDb.longitude = _longitude;
+        mColumnsInDb.setLocation(_latitide, _longitude);
     }
     public void setWidthHeight(int _width, int _height) {
-        mColumnsInDb.width = _width;
-        mColumnsInDb.height = _height;
+        mColumnsInDb.setWidthHeight(_width, _height);
     }
     public void setBucket (String bucketId, String bucketDisplayName) {
-        mColumnsInDb.bucket_id = bucketId;
-        mColumnsInDb.bucket_display_name = bucketDisplayName;
+        mColumnsInDb.setBucket(bucketId, bucketDisplayName);
     }
     public void setThumbnailMagic(int thumbnail) {
-        mColumnsInDb.mini_thumb_magic = thumbnail;
+        mColumnsInDb.setThumbnailMagic(thumbnail);
+
     }
     public void setOrientation (int orientation) {
-        mColumnsInDb.orientation = orientation;
+        mColumnsInDb.setOrientation(orientation);
     }
 
     @Override
     public String toString() {
 
-        DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String s = "";
 
-        String s ="";
-        s += "Columns:" + mColumnsInDb.mColumns;
-        s += ",ID:" + mColumnsInDb._id + ", Sz:" + mColumnsInDb._size;
-        s += ",Name:" + mColumnsInDb._display_name;
-        s += "\nMIME:" + mColumnsInDb.mime_type + ",Title:" +  mColumnsInDb.title;
-        if (mColumnsInDb.mColumns > 13) s += "\nTaken: " + df.format(mColumnsInDb.datetaken);
-        else s += "\nTaken: NA";
-        s += ",Added: " + df.format(mColumnsInDb.date_added);
-        s += ",Modified: " + df.format(mColumnsInDb.date_modified);
-        s += "\nDescription:" + mColumnsInDb.description;
-        s += "\nPicasaId:" + mColumnsInDb.picasa_id + ",Private:" + mColumnsInDb.isprivate;
-        if (mColumnsInDb.mColumns > 19)
-            s += ",{WH:(" + mColumnsInDb.width + "," + mColumnsInDb.height + ")}";
-        else s += ",{WH:(NA,NA)}";
-        if (mColumnsInDb.mColumns > 15) s += ",ThumbNail:" + mColumnsInDb.mini_thumb_magic;
-        else s += ",ThumbNail:NA";
-        if (mColumnsInDb.mColumns > 14) s += ",Orientation:" + mColumnsInDb.orientation;
-        else s += ",Orientation:NA";
-        if (mColumnsInDb.mColumns > 12)
-            s += "\nLocation:(" + mColumnsInDb.latitude +"," + mColumnsInDb.longitude + ")";
-        else
-            s += "\nLocation:(NA,NA)";
-        if (mColumnsInDb.mColumns > 16) s += "\nBktId:" + mColumnsInDb.bucket_id;
-        else s += "\nBktId:NA";
-        if (mColumnsInDb.mColumns > 17) s += ",BktDisName:" + mColumnsInDb.bucket_display_name;
-        else s += ",BktDisName:NA";
+        if (mImageFileName != null) s += "\n[IMAGEDB] Filename - " + mImageFileName.toString();
+        if (mColumnsInDb != null) s += mColumnsInDb; // Print DB columns
+        if (mError != null) s += mError; //Error in this module
+        if (mJpegExifData != null) s += mJpegExifData; // Read EXIF data
+
+        // Read MetaData from Library
+        if (mImageMetaData != null) {
+            for (Directory directory : mImageMetaData.getDirectories()) {
+                // Each Directory stores values in Tag objects
+                for (Tag tag : directory.getTags()) {
+                    s += tag.toString() + "\n";
+                }
+                // Each Directory may also contain error messages
+                if (directory.hasErrors()) {
+                    for (String error : directory.getErrors()) {
+                        s += "ERROR: " + error;
+                    }
+                } // end if
+            } // end for 'all directories'
+        }
+
         return s;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            mImageId = null;
+            mImageFileName = null;
+            mImageMetaData = null;
+            mJpegExifData = null;
+            mColumnsInDb = null;
+        } finally {
+            super.finalize();
+        }
     }
 }
--- app/src/main/java/com/pacificstreetventures/apps/picinfo/PicInfoMainActivity.java
+++ app/src/main/java/com/pacificstreetventures/apps/picinfo/PicInfoMainActivity.java
@@ -8,143 +8,82 @@
  */
 package com.pacificstreetventures.apps.picinfo;
 
-import android.content.ContentResolver;
-import android.database.Cursor;
-import android.database.CursorIndexOutOfBoundsException;
-import android.database.StaleDataException;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-
-public class PicInfoMainActivity extends ActionBarActivity {
-    private List<PicInfo> mPicsData = null;
+public class PicInfoMainActivity extends ActionBarActivity
+        implements ImageDataLoadFragment.OnFragmentInteractionListener {
     private final String TAG = getClass().getSimpleName();
-    private Thread mReadPicDbThread = null;
+    private List<PicInfo> mPicsData = null;
+    private final String FRAGMENT_DOWNLOAD_NAME = "IMAGE_DATA_DOWNLOAD_FRAGMENT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic_info_main);
+        Log.d(TAG, "onCreate");
 
-        // Upload the data
-        mPicsData = new ArrayList<>();
-        uploadImageDataThread(MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        uploadImageDataThread(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Setup list.
+        if (mPicsData == null)
+            mPicsData = new ArrayList<>();
+
+        // Create download fragment it does not exist
+        FragmentManager fMgr = getSupportFragmentManager();
+        ImageDataLoadFragment fragment = (ImageDataLoadFragment) fMgr.findFragmentByTag(FRAGMENT_DOWNLOAD_NAME);
+        // Create one if one does not exist
+        if (fragment == null) {
+            FragmentTransaction fragmentTransaction = fMgr.beginTransaction();
+            ImageDataLoadFragment dataLoadFragement = ImageDataLoadFragment.newInstance("1", "2");
+            fragmentTransaction.add(dataLoadFragement, FRAGMENT_DOWNLOAD_NAME)
+                    .commit();
+            Log.d(TAG, "Created RetainedFragment - ImageDataLoadFragment");
+        }
     }
 
-    private void    uploadImageDataThread(final Uri uri) {
-        // Read images from Media.
-        String[] projection = new String[]{
-                MediaStore.MediaColumns.DISPLAY_NAME
-                , MediaStore.MediaColumns.DATE_ADDED
-                , MediaStore.MediaColumns.MIME_TYPE
-        };
 
-        // Upload the data
-        mReadPicDbThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                final List<PicInfo> pix = new ArrayList<PicInfo>();
-                Cursor cr = null;
-                ContentResolver contentResolver = null;
-                try {
-                    contentResolver = getContentResolver();
-                    cr = contentResolver.query(uri, // uri
-                            null, // projection - Which columns to return.
-                            null, // WHERE clause.
-                            null, // WHERE clause value substitution
-                            MediaStore.MediaColumns.DATE_ADDED + " DESC" // Sort order.
-                            // "datetaken DESC"
-                    );
 
-                    int nPixData = cr.getCount();
-                    int COLUMNS = cr.getColumnCount();
-                    // if (nPixData > 0) nPixData = 10; // debug
 
-                    cr.moveToFirst();
-                    for (int i = 0; i < nPixData; i++) {
-                        // Check if thread is interrupted
-                        if (Thread.interrupted()) {
-                            throw new InterruptedException();
-                        }
-                        // Read each row of the DB and save columns data safely.
-                        // Assumption - column #s dont change across manufacturer
-                        try {
-                            PicInfo p = new PicInfo();
-                            // Get as much as as much available.
-                            p.setColumns(COLUMNS); // Store how much data is available -- handle different versions
-                            if (COLUMNS > 0) p.setID(cr.getLong(0));    // 'id' idx=0
-                            if (COLUMNS > 2) p.setSize(cr.getLong(2));  // '_size' idx=2
-                            if (COLUMNS > 3) p.setDisplayName(cr.getString(3)); // '_display_name' idx=3
-                            if (COLUMNS > 4) p.setMimeType(cr.getString(4)); // 'mime_type' idx=4
-                            if (COLUMNS > 5) p.setTitle(cr.getString(5)); // 'title' idx=5
-                            if (COLUMNS > 6) p.setDateAdded(cr.getLong(6) * 1000); // 'date_added' idx=6 in seconds
-                            if (COLUMNS > 7) p.setDateModified(cr.getLong(7) * 1000); // 'date_modified' idx=7
-                            if (COLUMNS > 8) p.setDescription(cr.getString(8)); // 'description' idx=8
-                            if (COLUMNS > 9) p.setPicasaId(cr.getString(9)); // 'picasa_id' idx=9
-                            if (COLUMNS > 10) p.setPrivate(cr.getInt(10));// 'isprivate' idx=10
-                            // 'latitude' idx=11, and 'longitude' idx=12
-                            if (COLUMNS > 12) p.setLocation(cr.getDouble(11), cr.getDouble(12));
-                            if (COLUMNS > 13) p.setDateTaken(cr.getLong(13)); // 'datetaken' idx=13
-                            if (COLUMNS > 14) p.setOrientation(cr.getInt(14)); // 'orientation' idx=14
-                            if (COLUMNS > 15) p.setThumbnailMagic(cr.getInt(15)); // 'mini_thumb_magic' idx=15 -- use with map
-                            // 'bucket_id' idx=16  and 'bucket_display_name' idx=17
-                            if (COLUMNS > 17) p.setBucket(cr.getString(16), cr.getString(17));
-                            if (COLUMNS > 19) p.setWidthHeight(cr.getInt(18), cr.getInt(19));
-                            pix.add(p);
-                            Log.d(TAG, "Image:" + i + ", Data:\n" + p);
-                        } catch (CursorIndexOutOfBoundsException ex) {
-                            Log.e(TAG,"Bad cursor index=" + i + "\n" , ex);
-                        } catch (StaleDataException staleEx) {
-                            Log.e(TAG,"Stale date:", staleEx);
-                        }
-                        cr.moveToNext(); // Next item
-                    }
-                    Log.d(TAG, "Uri:" + uri.toString());
-                    Log.d(TAG, "Count:" + nPixData + ", Fields: " + Arrays.toString(cr.getColumnNames()));
-                } catch (OutOfMemoryError ex) {
-                    Log.e(TAG,"OutOfMemoryError:", ex);
-                } catch (InterruptedException ex) {
-                    Log.e(TAG,"InterruptedException:", ex);
-                }
-                finally {
-                    if (cr != null) cr.close();
-                }
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
 
-                // All finished save it to public list
-                if (pix.size() > 0 ) { // something to add.
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mPicsData.addAll(pix);
-                        }
-                    });
-                }
-            }
-        });
-        mReadPicDbThread.start(); // Start thread
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+    }
 
-        // Internal Media
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        Log.d(TAG, "onRetainCustomNonConfigurationInstance");
+        return super.onRetainCustomNonConfigurationInstance();
+    }
+
+    @Override
+    public Object getLastCustomNonConfigurationInstance() {
+        Log.d(TAG, "getLastCustomNonConfigurationInstance");
+        return super.getLastCustomNonConfigurationInstance();
     }
 
     @Override
     protected void onDestroy() {
-        // Shut down the interrupts.
-        if ((mReadPicDbThread != null ) && (mReadPicDbThread.isAlive()) ) {
-            mReadPicDbThread.interrupt();
-        }
+        Log.d(TAG, "onDestroy");
         // Destroy.
         super.onDestroy();
     }
@@ -169,5 +108,16 @@ public class PicInfoMainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // Interface methods:
+    //    onDatabaseUpdated: to communicate with 'ImageDataLoadFragment'
+    //---------------------------------------------------------------------------------------------
+
+    @Override
+    public void onDatabaseUpdated(List<PicInfo> dataList) {
+        mPicsData = dataList; // update data list
+        // Inform adapter of changes
     }
 }
